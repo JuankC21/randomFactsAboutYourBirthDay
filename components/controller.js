6c1e4a49e8b9d2a3ed3f5277a7b27a7bd66893a5
Juan Camilo <juank@Juans-MacBook-Pro.local>
2022-03-02 16:25:55 -0500

    [TESTIBG] Printing request property '_header'

--- components/controller.js
+++ components/controller.js
@@ -31,7 +31,7 @@ async function getBirthdayInfo(month, day, year) {
     let requestObj = { dayStr: `${day}`, getCountry: "0", monthStr: `${month}`, yearStr: `${year}` }
     let mybirthdayfacts = await dependencies.axios.post('https://www.mybirthdayfacts.com/MBFService.asmx/FetchDayHeadlines', requestObj, { headers: { 'Content-Type': 'application/json' } })
     let famousBornOnBirthday = await dependencies.axios.get(`https://playback.fm/ajax/celebrity-birthdays?month=${month}&day=${day}`, { 'Content-Type': 'application/json', 'Host': "127.0.0.1" })
-
+    console.log('Inicio debug', famousBornOnBirthday.request._header, 'Fin debug')
     birthdayInfo['famous'] = {
         image: famousBornOnBirthday.data.img,
         names: famousBornOnBirthday.data.names[0],
